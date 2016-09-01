c8a48b610141ca524cc120d36e15cd819190c107
Eli Perelman <eli@eliperelman.com>
2016-09-01 14:38:44 -0500

    Adding single-run test for karma

--- packages/neutrino/commands/test/index.js
+++ packages/neutrino/commands/test/index.js
@@ -14,6 +14,10 @@ const browser = (config, args, done) => {
   karma.singleRun = !args.options.watch;
   karma.autoWatch = args.options.watch;
 
+  if (args.files) {
+    karma.files = args.files;
+  }
+
   new Server(karma, done).start();
 };
 
