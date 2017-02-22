b838a0da54a1c31bbc5a2f5845df1ba42fba31dc
Eli Perelman <eli@eliperelman.com>
2017-02-22 10:59:37 -0600

    Introduce lint base to make lint preset creation much easier (#41)

--- packages/neutrino-preset-airbnb-base/src/index.js
+++ packages/neutrino-preset-airbnb-base/src/index.js
@@ -1,58 +1,31 @@
-'use strict';
+const lint = require('neutrino-lint-base');
+const merge = require('deepmerge');
 
-const path = require('path');
+module.exports = neutrino => {
+  lint(neutrino);
+  neutrino.config.module
+    .rule('lint')
+    .loader('eslint', props => merge(props, {
+      options: {
+        baseConfig: {
+          extends: ['airbnb-base']
+        },
+        rules: {
+          // handled by babel rules
+          'new-cap': 'off',
 
-const MODULES = path.join(__dirname, '../node_modules');
+          // handled by babel rules
+          'object-curly-spacing': 'off',
 
-module.exports = ({ config }) => {
-  config
-    .module
-      .rule('lint')
-        .test(/\.(js|jsx)$/)
-        .pre()
-        .include(path.join(process.cwd(), 'src'))
-        .loader('eslint', require.resolve('eslint-loader'), {
-          failOnError: process.env.NODE_ENV !== 'development',
-          emitWarning: process.env.NODE_ENV !== 'development',
-          emitError: process.env.NODE_ENV !== 'development',
-          cwd: process.cwd(),
-          useEslintrc: false,
-          root: true,
-          plugins: ['babel'],
-          baseConfig: {
-            extends: ['airbnb-base']
-          },
-          envs: ['es6'],
-          parser: 'babel-eslint',
-          parserOptions: {
-            ecmaVersion: 2017,
-            sourceType: 'module',
-            ecmaFeatures: {
-              objectLiteralDuplicateProperties: false,
-              generators: true,
-              impliedStrict: true
-            }
-          },
-          settings: {},
-          globals: ['process'],
-          rules: {
-            // handled by babel rules
-            'new-cap': 'off',
+          // require a capital letter for constructors
+          'babel/new-cap': ['error', { newIsCap: true }],
 
-            // handled by babel rules
-            'object-curly-spacing': 'off',
+          // require padding inside curly braces
+          'babel/object-curly-spacing': ['error', 'always'],
 
-            // require a capital letter for constructors
-            'babel/new-cap': ['error', { newIsCap: true }],
-
-            // require padding inside curly braces
-            'babel/object-curly-spacing': ['error', 'always'],
-
-            // guard against awaiting async functions inside of a loop
-            'babel/no-await-in-loop': 'error'
-          }
-        });
-
-  config.resolve.modules.add(MODULES);
-  config.resolveLoader.modules.add(MODULES);
+          // guard against awaiting async functions inside of a loop
+          'babel/no-await-in-loop': 'error'
+        }
+      }
+    }));
 };
