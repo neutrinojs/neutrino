f6bcb7bf2084e49c029037faedae879eb46fd2bf
Constantine Genchevsky <const.gen@gmail.com>
2017-04-12 17:06:19 +0300

    Polyfills (#173)
    * Rdesign Advanced Customization examples to use Neutrino v5 fashion
    
    * Fix polyfills set to match only supported platforms
    
    * Remove 'tranform-regenerator' because according to the babel preset debug log it is already included
    
    * Fix indentation
    
    * Add VSCode local settings to the ignore list
    
    * Separate chunk for polyfills

--- packages/neutrino-preset-web/index.js
+++ packages/neutrino-preset-web/index.js
@@ -67,14 +67,13 @@ module.exports = (neutrino) => {
   neutrino.use(htmlTemplate, neutrino.options.html);
   neutrino.use(namedModules);
   neutrino.use(compileLoader, {
-    include: [neutrino.options.source, neutrino.options.tests],
+    include: [neutrino.options.source, neutrino.options.tests, require.resolve('./polyfills.js')],
     babel: {
       plugins: [require.resolve('babel-plugin-syntax-dynamic-import')],
       presets: [
         [require.resolve('babel-preset-env'), {
           modules: false,
           useBuiltIns: true,
-          include: ['transform-regenerator'],
           targets: neutrino.options.compile.targets
         }]
       ]
@@ -85,8 +84,10 @@ module.exports = (neutrino) => {
     .when(process.env.NODE_ENV !== 'test', () => neutrino.use(chunk))
     .target('web')
     .context(neutrino.options.root)
+    .entry('polyfill')
+      .add(require.resolve('./polyfills.js'))
+      .end()
     .entry('index')
-      .add(require.resolve('babel-polyfill'))
       .add(neutrino.options.entry)
       .end()
     .output
