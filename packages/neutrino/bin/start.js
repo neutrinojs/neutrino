fb9dabe812180174d57d8a7bcacdbaa34884b5d2
сgenchevsky <const.gen@gmail.com>
2017-04-29 11:33:25 +0300

    Detect if dev server is public and log the local network IP in the terminal

--- packages/neutrino/bin/start.js
+++ packages/neutrino/bin/start.js
@@ -1,35 +1,58 @@
 const { start } = require('../src');
 const ora = require('ora');
+const dns = require('dns');
+const os = require('os');
+
+const platformHostName = os.hostname();
+const whenIPReady = new Promise((done, failed) => {
+  dns.lookup(platformHostName, (err, ip) => {
+    if (err) {
+      failed(err);
+    } else {
+      done(ip);
+    }
+  });
+});
 
 module.exports = (middleware, options) => {
   const spinner = ora('Building project').start();
 
-  return start(middleware, options)
-    .fork((errors) => {
-      spinner.fail('Building project failed');
-      errors.forEach((err) => {
-        console.error(err.stack || err);
-        err.details && console.error(err.details);
-      });
+  return whenIPReady.then(ip => start(middleware, options).fork(
+      (errors) => {
+        spinner.fail('Building project failed');
+        errors.forEach((err) => {
+          console.error(err.stack || err);
+          err.details && console.error(err.details);
+        });
 
-      process.exit(1);
-    }, (compiler) => { // eslint-disable-line consistent-return
-      if (!compiler.options.devServer) {
-        return spinner.succeed('Build completed');
+        process.exit(1);
+      },
+      (compiler) => {
+        if (!compiler.options.devServer) {
+          return spinner.succeed('Build completed');
+        }
+
+        const { devServer } = compiler.options;
+        const protocol = devServer.https ? 'https' : 'http';
+        const { port } = devServer;
+        let { host } = devServer;
+
+        if (host === '0.0.0.0') {
+          host = ip;
+        }
+
+        spinner.succeed(
+          `Development server running on: ${protocol}://${host}:${port}`
+        );
+
+        const building = ora('Waiting for initial build to finish').start();
+
+        compiler.plugin('done', () => building.succeed('Build completed'));
+        compiler.plugin('compile', () => {
+          building.text = 'Source changed, re-compiling';
+          building.start();
+        });
+        return undefined;
       }
-
-      const { devServer } = compiler.options;
-      const protocol = devServer.https ? 'https' : 'http';
-      const { host, port } = devServer;
-
-      spinner.succeed(`Development server running on: ${protocol}://${host}:${port}`);
-
-      const building = ora('Waiting for initial build to finish').start();
-
-      compiler.plugin('done', () => building.succeed('Build completed'));
-      compiler.plugin('compile', () => {
-        building.text = 'Source changed, re-compiling';
-        building.start();
-      });
-    });
+    ));
 };
