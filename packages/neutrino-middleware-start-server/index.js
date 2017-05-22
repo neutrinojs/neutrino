592fe44ad577b5af3325539ea1365ac1cef6e91e
Christoph Benjamin Weber <kriswep@wetainment.com>
2017-05-22 21:30:44 +0200

    use start server with inspect conditionally

--- packages/neutrino-middleware-start-server/index.js
+++ packages/neutrino-middleware-start-server/index.js
@@ -1,3 +1,13 @@
 const StartServerPlugin = require('start-server-webpack-plugin');
 
-module.exports = ({ config }, options) => config.plugin('start-server').use(StartServerPlugin, [options.name]);
+module.exports = (neutrino, options) => {
+  if (neutrino.options.args && neutrino.options.args.debug) {
+    // start server with --inspect
+    return neutrino.config.plugin('start-server').use(StartServerPlugin, [{
+      name: options.name,
+      nodeArgs: ['--inspect']
+    }]);
+  }
+  // normal start-server
+  return neutrino.config.plugin('start-server').use(StartServerPlugin, [options.name]);
+};
