58adbb90c5a2cad3d4ea187b7797b7746f31e043
Eli Perelman <eli@eliperelman.com>
2017-06-03 10:38:43 -0500

    Caching based on hashed and inline manifest (#223)

--- packages/neutrino-middleware-chunk/index.js
+++ packages/neutrino-middleware-chunk/index.js
@@ -1,8 +1,21 @@
 const { CommonsChunkPlugin } = require('webpack').optimize;
+const ChunkHashPlugin = require('webpack-chunk-hash');
+const ChunkManifestPlugin = require('chunk-manifest-webpack-plugin');
 const merge = require('deepmerge');
 
 module.exports = ({ config }, options) => config
   .plugin('chunk')
-  .use(CommonsChunkPlugin, [
-    merge({ minChunks: Infinity, names: ['vendor', 'manifest'] }, options)
-  ]);
+    .use(CommonsChunkPlugin, [
+      merge({ minChunks: Infinity, names: ['vendor', 'manifest'] }, options.commons || {})
+    ])
+    .end()
+  .plugin('chunk-hash')
+    .use(ChunkHashPlugin)
+    .end()
+  .plugin('chunk-manifest')
+    .use(ChunkManifestPlugin, [
+      merge({
+        filename: 'chunk-manifest.json',
+        manifestVariable: 'webpackManifest'
+      }, options.manifest || {})
+    ]);
