3fb0bba03dcebfa991395474c4128b7a6ae623bf
Eli Perelman <eli@eliperelman.com>
2017-06-05 15:51:38 -0500

    Custom commands 😍

---
+++ packages/neutrino/bin/execute.js
@@ -0,0 +1,28 @@
+const { Neutrino } = require('../src');
+// const { defaultTo } = require('ramda');
+const merge = require('deepmerge');
+const ora = require('ora');
+
+module.exports = (middleware, args) => {
+  const commandName = args._[0];
+  const spinner = ora(`Running ${commandName}`).start();
+  const options = merge({
+    args,
+    debug: args.debug,
+    env: {
+      NODE_ENV: 'production'
+    }
+  }, args.options);
+  const api = Neutrino(options);
+
+  return api
+    .run(commandName, middleware, config => api.commands[commandName](config))
+    .fork((errors) => {
+      spinner.fail(`Running ${commandName} failed`);
+      errors.forEach(err => console.error(err));
+      process.exit(1);
+    }, (output) => {
+      spinner.succeed(`Running ${commandName} completed`);
+      console.log(output);
+    });
+};
