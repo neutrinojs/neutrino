bc602bfe58780ad0c446a9cb9a357e20bf8e5722
Eli Perelman <eli@eliperelman.com>
2017-06-05 14:49:11 -0500

    Switching to static directory for non-compiled/copyable assets (#235)

--- packages/neutrino-preset-react/index.js
+++ packages/neutrino-preset-react/index.js
@@ -17,7 +17,6 @@ module.exports = (neutrino, opts = {}) => {
         development: {
           plugins: [
             opts.hot !== false ? require.resolve('react-hot-loader/babel') : {},
-            require.resolve('babel-plugin-transform-class-properties'),
             require.resolve('babel-plugin-transform-es2015-classes')
           ]
         }
@@ -27,7 +26,7 @@ module.exports = (neutrino, opts = {}) => {
 
   neutrino.use(web, options);
 
-  config
+  neutrino.config
     .resolve
       .modules.add(MODULES).end()
       .extensions.add('.jsx').end()
