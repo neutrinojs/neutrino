f77317f0824055763d2081b9e4ebaabcde290c96
Lee Siong Chan <ahlee2326@me.com>
2017-10-08 17:50:07 +0800

    Fix neutrino-middleware-loader-merge when using with babel

--- packages/neutrino-middleware-loader-merge/index.js
+++ packages/neutrino-middleware-loader-merge/index.js
@@ -1,6 +1,7 @@
+const babelMerge = require('babel-merge');
 const merge = require('deepmerge');
 
 module.exports = (ruleId, loaderId) => ({ config }, options) => config.module
   .rule(ruleId)
   .use(loaderId)
-  .tap(opts => merge(opts, options));
+  .tap(opts => (loaderId === 'babel' ? babelMerge(opts, options) : merge(opts, options)));
