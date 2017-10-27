852e2c18fc51b9f11d84ad84ca41dc09e9df8089
Hassan Ali <helfi92@gmail.com>
2017-10-27 13:49:18 -0400

    Add success message

--- packages/create-neutrino-project/commands/init/generator/install.js
+++ packages/create-neutrino-project/commands/init/generator/install.js
@@ -20,6 +20,9 @@ module.exports = function() {
   ], { stdio: 'inherit' });
 
   command.on('close', () => {
+    this.log(`Success! Created ${this.options.directory} at ${process.cwd()}`);
+    this.log(`To get started, cd into the new directory: ${chalk.cyan(`cd ${this.options.directory}`)}`);
+
     return done();
   });
 };
