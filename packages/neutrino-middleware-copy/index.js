2beea0027f56ce029dc4858a771184f81f9d66de
Ed Morley <501702+edmorley@users.noreply.github.com>
2017-10-29 22:24:44 +0000

    Make --debug set copy-webpack-plugin's 'debug' option
    Setting copy-webpack-plugin's `debug` option to `true` increases the
    log level from `warning` to `info`:
    https://github.com/webpack-contrib/copy-webpack-plugin#available-options
    
    Which gives output of form:
    
    ```
    [copy-webpack-plugin] processing from: '**/*' to: 'static'
    [copy-webpack-plugin] begin globbing '...\src\static\**\*' with a context of 'C:\Users\Ed\src\_todo\neutrino-dev\src\static'
    [copy-webpack-plugin] determined that '.../src/static/foo.txt' should write to 'static/foo.txt'
    [copy-webpack-plugin] reading ...\src\static\foo.txt to write to assets
    [copy-webpack-plugin] writing 'static/foo.txt' to compilation assets from '...\src\static\foo.txt'
    ```
    
    Refs #389.

--- packages/neutrino-middleware-copy/index.js
+++ packages/neutrino-middleware-copy/index.js
@@ -1,10 +1,13 @@
 const CopyPlugin = require('copy-webpack-plugin');
 const merge = require('deepmerge');
 
-module.exports = ({ config }, opts = {}) => {
-  const { patterns, options } = merge({ patterns: [], options: {} }, opts);
+module.exports = (neutrino, opts = {}) => {
+  const { patterns, options } = merge({
+    patterns: [],
+    options: { debug: neutrino.options.debug }
+  }, opts);
 
-  config
+  neutrino.config
     .plugin(options.pluginId || 'copy')
     .use(CopyPlugin, [patterns, options]);
 };
