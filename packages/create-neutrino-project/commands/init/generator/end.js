6de29fde1fa828bbe3b3411ca4f736a9c7c70e8d
Hassan Ali <helfi92@gmail.com>
2017-10-31 10:16:40 -0400

    Put all methods inside the Generator class

--- packages/create-neutrino-project/commands/init/generator/end.js
+++
@@ -1,6 +0,0 @@
-const chalk = require('chalk');
-
-module.exports = function() {
-  this.log(`Success! Created ${this.options.directory} at ${process.cwd()}`);
-  this.log(`To get started, change your current working directory to: ${chalk.cyan(this.options.directory)}`);
-};
