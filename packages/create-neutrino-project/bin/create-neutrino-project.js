7aa3887ca30ef09bc995acc7ed0300f2929d931d
Hassan Ali <helfi92@gmail.com>
2017-11-01 08:46:50 -0400

    Switch template string to normal string

--- packages/create-neutrino-project/bin/create-neutrino-project.js
+++ packages/create-neutrino-project/bin/create-neutrino-project.js
@@ -8,11 +8,11 @@ const env = yeoman.createEnv();
 const done = exitCode => process.exit(exitCode || 0);
 const dir = path.resolve(__dirname, '../commands/init');
 
-env.register(require.resolve(dir), `create-neutrino-project:init`);
+env.register(require.resolve(dir), 'create-neutrino-project:init');
 
 const cli = yargs.command('<project-directory>')
   .demandCommand(1, 'Only <project-directory> is required')
   .help()
   .argv;
 
-env.run(`create-neutrino-project:init`, { directory: cli._[0] }, done);
+env.run('create-neutrino-project:init', { directory: cli._[0] }, done);
