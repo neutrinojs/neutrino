902177f5e6b8738d8f35e0d7a639858fa5cabadd
Fahad Hossain <fa7ad@users.noreply.github.com>
2017-11-18 20:54:36 +0600

    Add official preact preset to packages (#454)

---
+++ packages/preact/test/preact_test.js
@@ -0,0 +1,35 @@
+import test from 'ava';
+import { validate } from 'webpack';
+import { Neutrino } from 'neutrino';
+
+test('loads preset', t => {
+  t.notThrows(() => require('..'));
+});
+
+test('uses preset', t => {
+  const api = Neutrino();
+
+  t.notThrows(() => api.use(require('..')));
+});
+
+test('valid preset production', t => {
+  process.env.NODE_ENV = 'production';
+  const api = Neutrino();
+
+  api.use(require('..'));
+
+  const errors = validate(api.config.toConfig());
+
+  t.is(errors.length, 0);
+});
+
+test('valid preset development', t => {
+  process.env.NODE_ENV = 'development';
+  const api = Neutrino();
+
+  api.use(require('..'));
+
+  const errors = validate(api.config.toConfig());
+
+  t.is(errors.length, 0);
+});
