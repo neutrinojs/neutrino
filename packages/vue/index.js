7a79aa107ed8a39c2edeb97bc27a4067c70a1cf5
Hassan Ali <helfi92@gmail.com>
2017-11-22 12:42:25 -0500

    Start using neutrino.options.extensions

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -1,42 +1,34 @@
 const web = require('@neutrinojs/web');
 const path = require('path');
-const arrify = require('arrify');
 const merge = require('deepmerge');
 
-const LOADER_EXTENSIONS = /\.vue$/;
 const MODULES = path.join(__dirname, 'node_modules');
 
 module.exports = (neutrino, options) => {
   neutrino.use(web, options);
 
-  const lintRule = neutrino.config.module.rules.get('lint');
-
   neutrino.config.module.rule('vue')
-    .test(LOADER_EXTENSIONS)
+    .test(neutrino.options.extensions)
     .use('vue')
     .loader(require.resolve('vue-loader'))
     .options(options);
 
-  if (lintRule) {
-    // ensure conditions is an array of original values plus our own regex
-    const conditions = arrify(lintRule.get('test')).concat([LOADER_EXTENSIONS]);
-    neutrino.config.module
-      .rule('lint')
-      .test(conditions)
-      .use('eslint')
-      .tap(options => merge(options, {
-        plugins: ['vue'],
-        envs: ['node'],
-        parserOptions: {
-          ecmaFeatures: {
-            jsx: true
-          }
-        },
-        rules: {
-          'vue/jsx-uses-vars': 2
+  neutrino.config.when(neutrino.config.module.rules.has('lint'), () => neutrino.config.module
+    .rule('lint')
+    .test(neutrino.options.extensions)
+    .use('eslint')
+    .tap(options => merge(options, {
+      plugins: ['vue'],
+      envs: ['node'],
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
         }
-      }));
-  }
+      },
+      rules: {
+        'vue/jsx-uses-vars': 2
+      }
+    })));
 
   if (neutrino.config.plugins.has('stylelint')) {
     neutrino.config.plugin('stylelint')
