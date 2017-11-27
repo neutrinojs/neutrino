3ba030b34fca18ee5a866b2d8b33a8441f0f727b
Hassan Ali <helfi92@gmail.com>
2017-11-27 17:16:19 -0500

    Fix method signature

--- packages/neutrino/src/devServer.js
+++ packages/neutrino/src/devServer.js
@@ -2,7 +2,7 @@ const merge = require('deepmerge');
 const Future = require('fluture');
 const { serve, validate } = require('./webpack');
 
-// server :: Object config -> Future () Function
+// devServer :: (Object config -> Object api) -> Future () Function
 const devServer = (config, { options }) => Future
   .of(merge({ devServer: { host: 'localhost', port: 5000, noInfo: !options.debug } }, config))
   .chain(validate)
