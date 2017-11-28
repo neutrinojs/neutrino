1af6f86e7a9dbcbb25190d81e843405189e070a8
Hassan Ali <helfi92@gmail.com>
2017-11-28 09:35:07 -0500

    Add webpack bundle information when running in debug (#486)
    * Add webpack bundle information when running in debug
    
    refs: https://github.com/mozilla-neutrino/neutrino-dev/issues/389
    
    * Fix method signature

--- packages/neutrino/src/devServer.js
+++ packages/neutrino/src/devServer.js
@@ -2,9 +2,9 @@ const merge = require('deepmerge');
 const Future = require('fluture');
 const { serve, validate } = require('./webpack');
 
-// server :: Object config -> Future () Function
-const devServer = config => Future
-  .of(merge({ devServer: { host: 'localhost', port: 5000, noInfo: true } }, config))
+// devServer :: (Object config -> Object api) -> Future () Function
+const devServer = (config, { options }) => Future
+  .of(merge({ devServer: { host: 'localhost', port: 5000, noInfo: !options.debug } }, config))
   .chain(validate)
   .chain(serve);
 
