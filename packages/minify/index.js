54f3787060f0047e99315f4baedf20ea5afbf3d9
Tim Kelty <tim@fusionary.com>
2017-12-04 12:58:19 -0500

    Never pass true, {} instead

--- packages/minify/index.js
+++ packages/minify/index.js
@@ -1,11 +1,8 @@
 const babelMinify = require('@neutrinojs/babel-minify');
 const imageMinify = require('@neutrinojs/image-minify');
 
-module.exports = (neutrino, options = {
-  babel: {},
-  image: {}
-}) => {
+module.exports = (neutrino, options = {}) => {
   neutrino.config
-    .when(options.babel, () => neutrino.use(babelMinify, options.babel))
-    .when(options.image, () => neutrino.use(imageMinify, options.image));
+    .when(options.babel, () => neutrino.use(babelMinify, options.babel !== true && options.babel || {}))
+    .when(options.image, () => neutrino.use(imageMinify, options.image !== true && options.image || {}));
 }
