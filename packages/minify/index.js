7270595aa5930f831f6eedac5b68df638d897f84
Tim Kelty <tim@fusionary.com>
2017-12-05 14:54:19 -0500

    Merge options, minify by default when calling directly.

--- packages/minify/index.js
+++ packages/minify/index.js
@@ -1,7 +1,13 @@
+const merge = require('deepmerge');
 const babelMinify = require('@neutrinojs/babel-minify');
 const imageMinify = require('@neutrinojs/image-minify');
 
-module.exports = (neutrino, options = {}) => {
+module.exports = (neutrino, opts = {}) => {
+  const options = merge({
+    babel: {},
+    image: {}
+  }, opts);
+
   neutrino.config
     .when(options.babel, () => neutrino.use(babelMinify, options.babel))
     .when(options.image, () => neutrino.use(imageMinify, options.image));
