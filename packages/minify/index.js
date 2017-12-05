b9acabd8fb7624e5e3890be8c5bbd18d1c4df0c1
Tim Kelty <tim@fusionary.com>
2017-12-05 14:57:04 -0500

    Include style-minify in minify

--- packages/minify/index.js
+++ packages/minify/index.js
@@ -1,14 +1,17 @@
 const merge = require('deepmerge');
 const babelMinify = require('@neutrinojs/babel-minify');
 const imageMinify = require('@neutrinojs/image-minify');
+const styleMinify = require('@neutrinojs/style-minify');
 
 module.exports = (neutrino, opts = {}) => {
   const options = merge({
     babel: {},
-    image: {}
+    image: {},
+    style: {}
   }, opts);
 
   neutrino.config
     .when(options.babel, () => neutrino.use(babelMinify, options.babel))
-    .when(options.image, () => neutrino.use(imageMinify, options.image));
+    .when(options.image, () => neutrino.use(imageMinify, options.image))
+    .when(options.style, () => neutrino.use(styleMinify, options.style));
 }
