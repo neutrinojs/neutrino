16396d2baedcaf936079f7123c58905d7a3e2b0b
Eli Perelman <eli@eliperelman.com>
2017-12-06 10:26:16 -0600

    Remove reliance on babel environments, make babel options merging consistent (#548)

--- packages/preact/index.js
+++ packages/preact/index.js
@@ -1,5 +1,6 @@
-const web = require('@neutrinojs/web');
+const compileLoader = require('@neutrinojs/compile-loader');
 const loaderMerge = require('@neutrinojs/loader-merge');
+const web = require('@neutrinojs/web');
 const { join } = require('path');
 const merge = require('deepmerge');
 
@@ -8,35 +9,26 @@ const MODULES = join(__dirname, 'node_modules');
 module.exports = (neutrino, opts = {}) => {
   const options = merge({
     hot: true,
-    babel: {
-      plugins: [
-        [
-          require.resolve('babel-plugin-transform-react-jsx'),
-          { pragma: 'h' }
-        ],
-        [
-          require.resolve('babel-plugin-jsx-pragmatic'),
-          {
-            module: 'preact',
-            export: 'h',
-            import: 'h'
-          }
-        ],
-        require.resolve('babel-plugin-transform-object-rest-spread'),
-        process.env.NODE_ENV !== 'development' ?
-          [require.resolve('babel-plugin-transform-class-properties'), { spec: true }] :
-          {}
-      ],
-      env: {
-        development: {
-          plugins: [
-            [require.resolve('babel-plugin-transform-class-properties'), { spec: true }],
-            require.resolve('babel-plugin-transform-es2015-classes')
-          ]
-        }
-      }
-    }
+    babel: {}
   }, opts);
+
+  Object.assign(options, {
+    babel: compileLoader.merge({
+      plugins: [
+        [require.resolve('babel-plugin-transform-react-jsx'), { pragma: 'h' }],
+        [require.resolve('babel-plugin-jsx-pragmatic'), {
+          module: 'preact',
+          export: 'h',
+          import: 'h'
+        }],
+        require.resolve('babel-plugin-transform-object-rest-spread'),
+        [require.resolve('babel-plugin-transform-class-properties'), { spec: true }],
+        process.env.NODE_ENV === 'development'
+          ? require.resolve('babel-plugin-transform-es2015-classes')
+          : {}
+      ]
+    }, options.babel)
+  });
 
   neutrino.use(web, options);
 
