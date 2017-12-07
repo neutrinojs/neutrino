471ae7b52227cc59e41e3e75da54a2f47e0bd043
Eli Perelman <eli@eliperelman.com>
2017-12-07 10:36:59 -0600

    Fixes location of create-project bin when using yarn create

--- packages/create-project/bin/create-project.js
+++ packages/create-project/bin/create-project.js
@@ -8,7 +8,7 @@ const env = createEnv();
 const done = exitCode => process.exit(exitCode || 0);
 const dir = resolve(__dirname, '../commands/init');
 
-env.register(require.resolve(dir), 'create-neutrino-project');
+env.register(require.resolve(dir), 'create-project');
 
 const cli = yargs.command('<project-directory>')
   .demandCommand(1, 'Only <project-directory> is required')
@@ -17,8 +17,8 @@ const cli = yargs.command('<project-directory>')
 const directory = isAbsolute(cli._[0]) ? cli._[0] : join(process.cwd(), cli._[0]);
 const name = basename(directory);
 
-env.run('create-neutrino-project', {
+env.run('create-project', {
   directory,
   name,
-  stdio: process.env.NODE_ENV === 'test' ? 'ignore' : 'inherit'
+  stdio: 'ignore'
 }, done);
