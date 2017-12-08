6a08da6e40a59290c607a582312ff2692f76c02d
Eli Perelman <eli@eliperelman.com>
2017-12-07 20:15:28 -0600

    Remove buggy devserver compiler message debouncing

--- packages/neutrino/bin/start.js
+++ packages/neutrino/bin/start.js
@@ -1,5 +1,4 @@
 const ora = require('ora');
-const debounce = require('lodash.debounce');
 const { start } = require('../src');
 const base = require('./base');
 
@@ -55,17 +54,10 @@ module.exports = (middleware, args, cli) => {
         compiler.plugin('done', () => {
           building.succeed('Build completed');
         });
-
-        // We debounce the "compile" event due to the devServer emitting
-        // many events with over emitted file saves. Typically this would
-        // cause the terminal to become filled with "re-comiling" messages,
-        // But debouncing this limits the message to a rolling 1-second
-        // interval to capture related compile events, improving the
-        // terminal message usefulness.
-        compiler.plugin('compile', debounce(() => {
+        compiler.plugin('compile', () => {
           building.text = 'Source changed, re-compiling';
           building.start();
-        }, 1000, { leading: true, maxWait: 1000 }));
+        });
       }
     }
   });
