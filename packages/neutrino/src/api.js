db16011d95784e1aed236e39d3f655f2adb9f06f
Tim Kelty <timkelty@gmail.com>
2017-12-13 09:19:08 -0500

    Fix broken regex (#604)
    * Fix broken regex
    
    * String.raw tag fn

--- packages/neutrino/src/api.js
+++ packages/neutrino/src/api.js
@@ -155,7 +155,7 @@ class Api {
 
   // regexFromExtensions :: Array extensions -> RegExp
   regexFromExtensions(extensions = this.options.extensions) {
-    return new RegExp(`.(${extensions.join('|')})$`);
+    return new RegExp(String.raw`\.(${extensions.join('|')})$`);
   }
 
   // emit :: Any -> IO
