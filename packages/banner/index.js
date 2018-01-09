ffd9b15169254f53d31280eb75cf2d321679d1a7
Eli Perelman <eli@eliperelman.com>
2018-01-09 08:25:09 -0600

    Only install banner in files that are modules by default (#663)

--- packages/banner/index.js
+++ packages/banner/index.js
@@ -1,12 +1,15 @@
 const { BannerPlugin } = require('webpack');
 const merge = require('deepmerge');
 
-module.exports = ({ config }, options = {}) => config
-  .plugin(options.pluginId || 'banner')
-  .use(BannerPlugin, [
-    merge({
-      banner: 'require(\'source-map-support\').install();',
-      raw: true,
-      entryOnly: true
-    }, options)
-  ]);
+module.exports = (neutrino, options = {}) => {
+  neutrino.config
+    .plugin(options.pluginId || 'banner')
+    .use(BannerPlugin, [
+      merge({
+        banner: 'require(\'source-map-support\').install();',
+        test: neutrino.regexFromExtensions(),
+        raw: true,
+        entryOnly: true
+      }, options)
+    ]);
+};
