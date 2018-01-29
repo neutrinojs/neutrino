566a89ee05ac2cb8601bced35c63e09377ebf2ff
Eli Perelman <eli@eliperelman.com>
2018-01-29 12:35:33 -0600

    Correctly merge eslint rules, fix hardcoded formatter

--- packages/airbnb-base/index.js
+++ packages/airbnb-base/index.js
@@ -1,25 +1,22 @@
 const lint = require('@neutrinojs/eslint');
-const merge = require('deepmerge');
 
 module.exports = (neutrino, opts = {}) => {
-  neutrino.use(lint, merge.all([
-    {
-      eslint: {
-        baseConfig: {
-          extends: ['airbnb-base']
-        },
-        rules: {
-          // handled by babel rules
-          'new-cap': 'off',
-          // handled by babel rules
-          'object-curly-spacing': 'off',
-          // require a capital letter for constructors
-          'babel/new-cap': ['error', { newIsCap: true }],
-          // require padding inside curly braces
-          'babel/object-curly-spacing': ['error', 'always']
-        }
+  neutrino.use(lint, lint.merge({
+    eslint: {
+      baseConfig: {
+        extends: ['airbnb-base']
+      },
+      rules: {
+        // handled by babel rules
+        'new-cap': 'off',
+        // handled by babel rules
+        'object-curly-spacing': 'off',
+        // require a capital letter for constructors
+        'babel/new-cap': ['error', { newIsCap: true }],
+        // require padding inside curly braces
+        'babel/object-curly-spacing': ['error', 'always']
       }
-    },
-    opts
-  ]));
+    }
+  },
+  opts));
 };
--- packages/standardjs/index.js
+++ packages/standardjs/index.js
@@ -1,26 +1,23 @@
 const lint = require('@neutrinojs/eslint');
-const merge = require('deepmerge');
 
 module.exports = (neutrino, opts = {}) => {
-  neutrino.use(lint, merge.all([
-    {
-      eslint: {
-        baseConfig: {
-          extends: ['standard', 'standard-jsx']
-        },
-        plugins: ['standard'],
-        rules: {
-          // handled by babel rules
-          'new-cap': 'off',
-          // handled by babel rules
-          'object-curly-spacing': 'off',
-          // require a capital letter for constructors
-          'babel/new-cap': ['error', { newIsCap: true }],
-          // require padding inside curly braces
-          'babel/object-curly-spacing': ['error', 'always']
-        }
+  neutrino.use(lint, lint.merge({
+    eslint: {
+      baseConfig: {
+        extends: ['standard', 'standard-jsx']
+      },
+      plugins: ['standard'],
+      rules: {
+        // handled by babel rules
+        'new-cap': 'off',
+        // handled by babel rules
+        'object-curly-spacing': 'off',
+        // require a capital letter for constructors
+        'babel/new-cap': ['error', { newIsCap: true }],
+        // require padding inside curly braces
+        'babel/object-curly-spacing': ['error', 'always']
       }
-    },
-    opts
-  ]));
+    }
+  },
+  opts));
 };
