b4ce8b1db77e955337ab2807c5ee73ee8ccdf8a2
Ed Morley <501702+edmorley@users.noreply.github.com>
2018-03-10 22:01:48 +0000

    Stop passing invalid 'name' option to html-loader
    Since it's not an option `html-loader` has ever recognised.
    
    Fixes #681.

--- packages/html-loader/index.js
+++ packages/html-loader/index.js
@@ -1,8 +1,6 @@
-const merge = require('deepmerge');
-
 module.exports = (neutrino, options = {}) => neutrino.config.module
   .rule('html')
   .test(neutrino.regexFromExtensions(['html']))
   .use('html')
     .loader(require.resolve('html-loader'))
-    .options(merge({ name: '[name].[ext]' }, options));
+    .options(options);
