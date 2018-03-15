598fe79481891e57920ed0c2b89f21aba2213260
Fredrik Teschke <f@ftes.de>
2018-03-15 09:52:44 +0100

    add missing space char

--- packages/env/index.js
+++ packages/env/index.js
@@ -2,4 +2,4 @@ const { EnvironmentPlugin } = require('webpack');
 
 module.exports = ({ config }, envs = []) => config
   .plugin('env')
-  .use(EnvironmentPlugin, Array.isArray(envs) ? ['NODE_ENV', ...envs]: { 'NODE_ENV': 'development', ...envs });
+  .use(EnvironmentPlugin, Array.isArray(envs) ? ['NODE_ENV', ...envs] : { 'NODE_ENV': 'development', ...envs });
