442e2f80f660024fe784eb8ea1aed91ff30ab73b
Ed Morley <501702+edmorley@users.noreply.github.com>
2018-04-20 13:41:29 +0100

    Add more tests for the web/node/library presets
    There's still a lot left untested, but this improves coverage
    significantly in the meantime.

--- packages/node/test/node_test.js
+++ packages/node/test/node_test.js
@@ -2,6 +2,8 @@ import test from 'ava';
 import { validate } from 'webpack';
 import { Neutrino } from 'neutrino';
 
+const expectedExtensions = ['.js', '.jsx', '.vue', '.ts', '.tsx', '.mjs', '.json'];
+
 test('loads preset', t => {
   t.notThrows(() => require('..'));
 });
@@ -20,21 +22,67 @@ test('uses preset with custom main', t => {
 });
 
 test('valid preset production', t => {
-  const api = Neutrino({ env: { NODE_ENV: 'production' } });
-  
+  const api = Neutrino({
+    command: 'build',
+    env: { NODE_ENV: 'production' }
+  });
   api.use(require('..'));
+  const config = api.config.toConfig();
 
-  const errors = validate(api.config.toConfig());
+  // Common
+  t.is(config.target, 'node');
+  t.deepEqual(config.resolve.extensions, expectedExtensions);
+  t.is(config.optimization, undefined);
+  t.is(config.devServer, undefined);
 
+  // NODE_ENV/command specific
+  t.is(config.mode, 'production');
+  t.is(config.devtool, 'source-map');
+
+  const errors = validate(config);
   t.is(errors.length, 0);
 });
 
 test('valid preset development', t => {
-  const api = Neutrino({ 'env': { NODE_ENV: 'development' } });
-
+  const api = Neutrino({
+    command: 'start',
+    env: { NODE_ENV: 'development' }
+  });
   api.use(require('..'));
+  const config = api.config.toConfig();
 
-  const errors = validate(api.config.toConfig());
+  // Common
+  t.is(config.target, 'node');
+  t.deepEqual(config.resolve.extensions, expectedExtensions);
+  t.is(config.optimization, undefined);
+  t.is(config.devServer, undefined);
 
+  // NODE_ENV/command specific
+  t.is(config.mode, 'development');
+  t.is(config.devtool, 'inline-sourcemap');
+
+  const errors = validate(config);
+  t.is(errors.length, 0);
+});
+
+test('valid preset test', t => {
+  const api = Neutrino({
+    command: 'test',
+    env: { NODE_ENV: 'test' }
+  });
+  api.use(require('..'));
+  const config = api.config.toConfig();
+
+  // Common
+  t.is(config.target, 'node');
+  t.deepEqual(config.resolve.extensions, expectedExtensions);
+  t.is(config.optimization, undefined);
+  t.is(config.devServer, undefined);
+
+  // NODE_ENV/command specific
+  t.is(config.mode, 'development');
+  t.is(config.devtool, 'source-map');
+
+  const errors = validate(config);
   t.is(errors.length, 0);
 });
