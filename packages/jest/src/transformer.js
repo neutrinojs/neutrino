97c350d6166831cb1276c68ed684138c4cb471b3
Ed Morley <501702+edmorley@users.noreply.github.com>
2018-05-04 11:41:57 +0100

    Switch from Object.assign() to rest/spread in Jest transformer

--- packages/jest/src/transformer.js
+++ packages/jest/src/transformer.js
@@ -5,13 +5,9 @@ module.exports = {
   // https://github.com/facebook/jest/blob/v22.4.2/packages/babel-jest/src/index.js#L105-L147
   // And is required due to:
   // https://github.com/facebook/jest/issues/1468
-  // TODO: See if it would be easier to switch to the higher-level babel-jest,
-  // and wrap that instead.
+  // TODO: See if it would be easier to wrap the higher-level babel-jest instead.
   process(src, filename, config) {
     // Babel 7 returns null if the file was ignored.
-    return transform(
-      src,
-      Object.assign({}, { filename }, config.globals.BABEL_OPTIONS)
-    ) || src;
+    return transform(src, { filename, ...config.globals.BABEL_OPTIONS }) || src;
   }
 };
