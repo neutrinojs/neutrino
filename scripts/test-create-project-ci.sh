49dec2a4f95693d19f1735db285b41799a08ea90
Eli Perelman <eli@eliperelman.com>
2018-05-17 12:10:17 -0500

    Set yarn auth token in env

--- scripts/test-create-project-ci.sh
+++ scripts/test-create-project-ci.sh
@@ -2,10 +2,12 @@
 
 set -euo pipefail
 
+export SKIP_CHANGELOG=true
+export YARN_AUTH_TOKEN="//registry.npmjs.org/:_authToken=token"
 yarn verdaccio --config verdaccio.yml & sleep 10
 yarn config set registry "http://localhost:4873"
 npm config set registry "http://localhost:4873"
-SKIP_CHANGELOG=true yarn lerna publish \
+yarn lerna publish \
   --force-publish=* \
   --skip-git \
   --registry http://localhost:4873/ \
