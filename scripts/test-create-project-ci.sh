7675fc4197132153fc2c352f6657acaa4ef3a102
Eli Perelman <eli@eliperelman.com>
2018-05-17 12:17:44 -0500

    Set fake token against localhost

--- scripts/test-create-project-ci.sh
+++ scripts/test-create-project-ci.sh
@@ -3,7 +3,8 @@
 set -euo pipefail
 
 export SKIP_CHANGELOG=true
-export YARN_AUTH_TOKEN="//registry.npmjs.org/:_authToken=token"
+export YARN_AUTH_TOKEN="//localhost:4873/:_authToken=token"
+
 yarn verdaccio --config verdaccio.yml & sleep 10
 yarn config set registry "http://localhost:4873"
 npm config set registry "http://localhost:4873"
