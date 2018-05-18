9e73636cb0fe66967452c049fe62a3ba1c52b4d1
Ed Morley <501702+edmorley@users.noreply.github.com>
2018-05-18 21:33:52 +0100

    Misc monorepo testing improvements (#884)
    * Ensure the monorepo .neutrinorc.js is included in linting
    * Remove unused 'random' package.json lifecycle script
    * Add --registry option to create-project
    * Remove unnecessary 'yarn config set registry'
    * Fix shebang whitespace
    * Adjust settings used for lerna publish in CI
    * Make verdaccio more resilient to NPM flakiness
    * Remove neutrino packages from Travis yarn cache after testing

--- scripts/test-create-project-ci.sh
+++ scripts/test-create-project-ci.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 set -euo pipefail
 
@@ -7,19 +7,27 @@ export YARN_AUTH_TOKEN="//localhost:4873/:_authToken=token"
 
 # Start verdaccio registry proxy in the background
 yarn verdaccio --config verdaccio.yml &
-yarn config set registry "http://localhost:4873"
 
 # Verdaccio isn't ready to immediately accept connections, so we need to wait
 while ! nc -zw 1 localhost 4873; do sleep 1; done
 
-# Publish all monorepo packages to the verdaccio registry
-yarn lerna publish \
-  --force-publish=* \
-  --skip-git \
+# Publish all monorepo packages to the verdaccio registry.
+# Canary mode ensures no git or working directory changes are made, but we
+# have to override the tag from `canary` to `latest` so create-project works.
+# The minor version will be bumped and a `-alpha.GIT_SHA` suffix added, which
+# should help avoid errors from clashing with an already-published version.
+yarn release \
   --registry http://localhost:4873/ \
   --yes \
-  --cd-version major
+  --canary \
+  --npm-tag latest
 
 # Run the integration tests, which will install packages
 # from the verdaccio registry
 yarn test:create-project
+
+# Remove cached Neutrino packages to avoid Travis cache churn.
+# Not using `yarn cache clean` since it doesn't support globs,
+# and does nothing more than call `fs.unlink(dir)` anyway.
+YARN_CACHE_DIR="$(yarn cache dir)"
+rm -rf "${YARN_CACHE_DIR}"/*-neutrino-*/ "${YARN_CACHE_DIR}"/*-@neutrinojs/
