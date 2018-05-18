e75a07761c784dfd685d0925597761a463215ebf
Ed Morley <501702+edmorley@users.noreply.github.com>
2018-05-18 15:03:00 +0100

    Remove unnecessary 'yarn config set registry'
    Since `create-project/test/cli_test.js` passes the registry setting
    itself, so it's not necessary to change the global setting. This
    saves having to remember to change the setting back if running
    the `test-create-project-ci.sh` script locally.

--- scripts/test-create-project-ci.sh
+++ scripts/test-create-project-ci.sh
@@ -7,7 +7,6 @@ export YARN_AUTH_TOKEN="//localhost:4873/:_authToken=token"
 
 # Start verdaccio registry proxy in the background
 yarn verdaccio --config verdaccio.yml &
-yarn config set registry "http://localhost:4873"
 
 # Verdaccio isn't ready to immediately accept connections, so we need to wait
 while ! nc -zw 1 localhost 4873; do sleep 1; done
