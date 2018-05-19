878211db2d89c5bd54c11d2bba9c0b3f691aceba
patiboh <patiboh@users.noreply.github.com>
2018-05-19 20:04:52 +0200

    Enable eslint-plugin-babel's babel/quotes rule (#874)

--- packages/airbnb/index.js
+++ packages/airbnb/index.js
@@ -10,11 +10,15 @@ module.exports = (neutrino, opts = {}) => {
         // handled by babel rules
         'new-cap': 'off',
         // handled by babel rules
+        'quotes': 'off',
+        // handled by babel rules
         'object-curly-spacing': 'off',
         // require a capital letter for constructors
         'babel/new-cap': ['error', { newIsCap: true }],
         // require padding inside curly braces
-        'babel/object-curly-spacing': ['error', 'always']
+        'babel/object-curly-spacing': ['error', 'always'],
+        // require single quotes for strings (with support for JSX Fragment syntax)
+        'babel/quotes': ['error', 'single', { avoidEscape: true }]
       }
     }
   },
