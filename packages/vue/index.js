0bac69daf6b73479b1853c0cbc1f31c95038c99f
Eli Perelman <eli@eliperelman.com>
2018-06-15 13:54:25 -0700

    Replace style-loader with vue-style-loader for vue

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -6,14 +6,20 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 module.exports = (neutrino, opts = {}) => {
   const options = merge({
     style: {
-      loaders: [{
-        loader: require.resolve('vue-style-loader'),
-        useId: 'vue'
-      }]
+      ruleId: 'style',
+      styleUseId: 'style',
+      exclude: [],
+      modulesTest: neutrino.regexFromExtensions(['css']),
+      modulesSuffix: ''
     }
   }, opts);
 
   neutrino.use(web, options);
+
+  neutrino.config.module
+    .rule(options.style.ruleId)
+    .use(options.style.styleUseId)
+    .loader(require.resolve('vue-style-loader'));
 
   neutrino.config.module
     .rule('vue')
@@ -22,6 +28,14 @@ module.exports = (neutrino, opts = {}) => {
         .loader(require.resolve('vue-loader'));
 
   neutrino.config.plugin('vue').use(VueLoaderPlugin);
+
+  if (neutrino.config.module.rules.has('compile')) {
+    neutrino.config.module
+      .rule('compile')
+      .test(neutrino.regexFromExtensions(
+        neutrino.options.extensions.filter(ext => ext !== 'vue'))
+      );
+  }
 
   neutrino.config.when(neutrino.config.module.rules.has('lint'), () => {
     neutrino.use(loaderMerge('lint', 'eslint'), {
