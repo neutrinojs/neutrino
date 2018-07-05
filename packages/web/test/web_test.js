dab9a4dd553c74468b838da1ab625738bae7b0b2
Ed Morley <501702+edmorley@users.noreply.github.com>
2018-07-05 13:16:39 +0100

    Simplify require()s in web_test.js

--- packages/web/test/web_test.js
+++ packages/web/test/web_test.js
@@ -2,21 +2,22 @@ import test from 'ava';
 import { validate } from 'webpack';
 import Neutrino from '../../neutrino/Neutrino';
 
+const mw = () => require('..');
 const expectedExtensions = ['.js', '.jsx', '.vue', '.ts', '.tsx', '.mjs', '.json'];
 
 test('loads preset', t => {
-  t.notThrows(() => require('..'));
+  t.notThrows(mw);
 });
 
 test('uses preset', t => {
-  t.notThrows(() => new Neutrino().use(require('..')));
+  t.notThrows(() => new Neutrino().use(mw()));
 });
 
 test('valid preset production', t => {
   const api = new Neutrino();
 
   api.config.mode('production');
-  api.use(require('..'));
+  api.use(mw());
   const config = api.config.toConfig();
 
   // Common
@@ -40,7 +41,7 @@ test('valid preset development', t => {
   const api = new Neutrino();
 
   api.config.mode('development');
-  api.use(require('..'));
+  api.use(mw());
   const config = api.config.toConfig();
 
   // Common
@@ -67,20 +68,20 @@ test('valid preset development', t => {
 test('throws when minify.babel defined', async t => {
   const api = new Neutrino();
 
-  const err = t.throws(() => api.use(require('..'), { minify: { babel: false } }));
+  const err = t.throws(() => api.use(mw(), { minify: { babel: false } }));
   t.true(err.message.includes('The minify.babel option has been removed'));
 });
 
 test('throws when minify.image defined', async t => {
   const api = new Neutrino();
 
-  const err = t.throws(() => api.use(require('..'), { minify: { image: true } }));
+  const err = t.throws(() => api.use(mw(), { minify: { image: true } }));
   t.true(err.message.includes('The minify.image option has been removed'));
 });
 
 test('throws when minify.style defined', async t => {
   const api = new Neutrino();
 
-  const err = t.throws(() => api.use(require('..'), { minify: { style: false } }));
+  const err = t.throws(() => api.use(mw(), { minify: { style: false } }));
   t.true(err.message.includes('The minify.style option has been removed'));
 });
