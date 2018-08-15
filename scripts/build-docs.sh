d91e91453562b0834b4cd877c69c279656d3aa16
Ed Morley <501702+edmorley@users.noreply.github.com>
2018-08-15 14:30:52 +0100

    Docs: Use mkdocs native external links support instead of redirects
    The workaround is no longer required, since mkdocs now supports using
    external links in the `nav` configuration:
    https://www.mkdocs.org/about/release-notes/#internal-refactor-of-pages-files-and-navigation
    https://www.mkdocs.org/user-guide/configuration/#nav
    
    This also required a tightening of the regex in build docs, so the
    "convert URLs to relative paths" step didn't also now catch the new
    absolute URL used for the "v8 Documentation" nav link.

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -11,4 +11,6 @@ mkdocs build
 # which have to use absolute URLs so that they work on the NPM package
 # pages. This converts them to site-root relative URLs, so that links
 # between packages target the current branch's version of the docs.
-find build/ -type f -name '*.html' -exec sed -i 's|https://neutrinojs\.org/|/|g' {} +
+# https://neutrinojs.org/ -> Not changed (since used for the "current version" link)
+# https://neutrinojs.org/foo/ -> /foo/
+find build/ -type f -name '*.html' -exec sed -ri 's|https://neutrinojs\.org/(\w+)|/\1|g' {} +
