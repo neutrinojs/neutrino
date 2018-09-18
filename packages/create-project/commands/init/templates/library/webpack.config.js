1cae38b58294194d1f340543592ef254f4e35537
Ed Morley <501702+edmorley@users.noreply.github.com>
2018-09-18 11:57:34 +0100

    Add a note to create-project's template webpack.config.js
    This intentionally doesn't list exact commands to run for inspect,
    since they may get out of date with reality (given that we can't
    update this file once projects have already been created).

--- packages/create-project/commands/init/templates/library/webpack.config.js
+++ packages/create-project/commands/init/templates/library/webpack.config.js
@@ -1,3 +1,6 @@
+// Whilst the configuration object can be modified here, the recommended way of making
+// changes is via the presets' options or Neutrino's API in `.neutrinorc.js` instead.
+// Neutrino's inspect feature can be used to view/export the generated configuration.
 const neutrino = require('neutrino');
 
 module.exports = neutrino().webpack();
