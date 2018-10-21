ff30cd66cdbdde6a17fe884f718728b88c498f32
Ed Morley <501702+edmorley@users.noreply.github.com>
2018-10-21 22:01:32 +0100

    jest: Use babel-jest instead of a custom implementation (#1183)
    We were previously just reimplementing parts of it, and not using
    the latest recommended Babel settings (such as still setting
    `retainLines: true` when sourcemaps are supported natively).
    
    There isn't currently a way to set the babel-jest `createTransformer()`
    options via Jest configuration/globals, so we instead have to resort
    to using an environment variable. This has the limitation of only
    supporting Babel options that are serializable (virtually all commonly
    used configuration options are), with any others being silently omitted.
    However this was already the case when using `globals` anyway.
    
    See:
    https://github.com/facebook/jest/blob/v23.6.0/packages/babel-jest/README.md
    https://github.com/facebook/jest/blob/v23.6.0/packages/babel-jest/src/index.js
    
    Fixes #851.

--- packages/jest/src/transformer.js
+++ packages/jest/src/transformer.js
@@ -1,13 +1,5 @@
-const { transform } = require('@babel/core');
+const { createTransformer } = require('babel-jest');
 
-module.exports = {
-  // This is inspired by:
-  // https://github.com/facebook/jest/blob/v22.4.2/packages/babel-jest/src/index.js#L105-L147
-  // And is required due to:
-  // https://github.com/facebook/jest/issues/1468
-  // TODO: See if it would be easier to wrap the higher-level babel-jest instead.
-  process(src, filename, config) {
-    // Babel 7 returns null if the file was ignored.
-    return transform(src, { filename, ...config.globals.BABEL_OPTIONS }) || src;
-  }
-};
+const babelOptions = JSON.parse(process.env.JEST_BABEL_OPTIONS);
+
+module.exports = createTransformer(babelOptions);
