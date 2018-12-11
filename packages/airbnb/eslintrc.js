31988f61463c8b20b3d5bef5fe67acabc3c0be7b
Eli Perelman <eli@eliperelman.com>
2018-12-10 18:38:10 -0600

    Fixes eslintrc validation errors

--- packages/airbnb/eslintrc.js
+++ packages/airbnb/eslintrc.js
@@ -2,7 +2,7 @@ const neutrino = require('../neutrino');
 const middleware = require('.');
 
 module.exports = neutrino({
-  use: middleware,
+  use: middleware(),
   options: {
     root: __dirname
   }
