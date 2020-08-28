623fc8b1f703a677d98734785e0528297eea77e8
babbage <601728820@qq.com>
2020-08-28 15:39:53 +0800

    feat: the inspect command supports specifying a configuration file
    #1609

--- packages/neutrino/bin/neutrino.js
+++ packages/neutrino/bin/neutrino.js
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
 
 const yargsParser = require('yargs-parser');
+const { join } = require('path');
 const neutrino = require('..');
 
 const argv = yargsParser(process.argv.slice(2));
 
 if (argv.inspect) {
+  if (argv.config) {
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    const middleware = require(join(process.cwd(), argv.config));
+    neutrino(middleware).inspect();
+    process.exit();
+  }
   neutrino().inspect();
   process.exit();
 }
@@ -17,6 +24,8 @@ https://neutrinojs.org/migration-guide/
 
 You may still inspect the generated webpack configuration using:
 neutrino --inspect --mode {production,development}
+or inspect with custom config
+neutrino --inspect --mode {production,development} --config .custom.neutrino.config
 `);
 
 process.exit(1);
