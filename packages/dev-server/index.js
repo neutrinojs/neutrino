b1d706dcb108dda35ddfc0ff0e2ce50580f06559
Nicolas Martel <nicolas.martel@onespan.com>
2021-10-28 08:54:00 -0400

    fix: dev-server default config

--- packages/dev-server/index.js
+++ packages/dev-server/index.js
@@ -6,14 +6,9 @@ module.exports =
       hot: true,
       // Redirect 404s to index.html, so that apps that use the HTML 5 History API work.
       historyApiFallback: true,
-      // Display any webpack compile errors (but not warnings) on an in-page overlay.
-      overlay: true,
-      // Only display compile duration and errors/warnings, to reduce noise when rebuilding.
-      stats: {
-        all: false,
-        errors: true,
-        timings: true,
-        warnings: true,
+      client: {
+        // Display any webpack compile errors (but not warnings) on an in-page overlay.
+        overlay: true,
       },
       ...options,
     });
